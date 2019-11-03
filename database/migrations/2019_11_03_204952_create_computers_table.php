83b01108733f2878c106a126952b7a8e376af59c
Gabriela <30907973+gmmonsalve@users.noreply.github.com>
2019-11-03 16:47:01 -0500

    computer model

---
+++ database/migrations/2019_11_03_204952_create_computers_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateComputersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('computers', function (Blueprint $table) {
+            $table->Increments('id');
+            $table->boolean('is_available');
+            $table->bigInteger('useroomid')->unsigned();
+            $table->foreign('useroomid')
+                  ->references('id')->on('user_rooms')
+                  ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('computers');
+    }
+}
