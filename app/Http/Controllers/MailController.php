1f0258b55e3dadf4402526ea7e24a56bc3151764
Gabriela <gmmonsalve@uninorte.edu.co>
2019-11-16 16:17:50 -0500

    Función de envío email

---
+++ app/Http/Controllers/MailController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use Mail;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+class MailController extends Controller {
+   public function basic_email() {
+      $headers = 'From: Toki @';
+      $user = Auth::user();
+      $to = $user ->email;
+      $name = $user->name;
+      mail($to,"¡Hola $name!","Esto es una prueba o(≧▽≦)o",$headers);
+      return redirect()->route('profile');;
+   }
+   
+}
